a486e95c3b71a3b9a304939aedb4da56b7b0fbc9
Vitaly Louchy <vitaly.louchy@indev.by>
2025-10-22 13:39:42 +0300

    tests logic fix

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -23,8 +23,8 @@ func GetConfig() Config {
 
 func InitConfig() {
 	config := Config{}
-	getEnv("DATABASE_DSN", "d", "postgres://postgres:root@localhost:5001/gofermart?sslmode=disable", "file storage path ", &config.DatabaseDNS)
-	getEnv("ADDRESS", "a", ":8080", "Server address", &config.Address)
+	getEnv("DATABASE_URI", "d", "postgres://postgres:root@localhost:5001/gofermart?sslmode=disable", "file storage path ", &config.DatabaseDNS)
+	getEnv("RUN_ADDRESS", "a", ":8080", "Server address", &config.Address)
 	getEnv("PRIVATE_KEY", "p", "Ci0tLS0tQkVHSU4gRUMgUFJJVkFURSBLRVktLS0tLQpNSGNDQVFFRUlBaDVxQTNybXFRUXV1MHZiS1YvK3pvdXoveS9JeTJwTHBJY1dVU3lJbVN3b0FvR0NDcUdTTTQ5CkF3RUhvVVFEUWdBRVlENTRWL3ZwKzU0UDlEWGFyWXF4NE1QY20rSEtSSVF6TmFzWVNvUlFIUS82UzZQczh0cE0KY1QrS3ZJSUM4Vy9lOWswVzdDbTcyTTFQOWpVN1NMZi92Zz09Ci0tLS0tRU5EIEVDIFBSSVZBVEUgS0VZLS0tLS0K", "Private Key", &config.PrivateKey)
 	getEnv("PUBLIC_KEY", "b", "Ci0tLS0tQkVHSU4gUFVCTElDIEtFWS0tLS0tCk1Ga3dFd1lIS29aSXpqMENBUVlJS29aSXpqMERBUWNEUWdBRVlENTRWL3ZwKzU0UDlEWGFyWXF4NE1QY20rSEsKUklRek5hc1lTb1JRSFEvNlM2UHM4dHBNY1QrS3ZJSUM4Vy9lOWswVzdDbTcyTTFQOWpVN1NMZi92Zz09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=", "Public Key", &config.PublicKey)
 	getEnv("ACCRUAL_SYSTEM_ADDRESS", "r", "localhost:8080", "address", &config.AccuralSystemAddress)
